bb2a1cfe56c4eb4d92f82feecec0001281145933
Alexey Ivasyuta <ivasyutaalexey@gmail.com>
2019-05-07 23:08:02 +0300

    fix bin args

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
+import gendiff from '../index';
+
 const program = require('commander');
 
 program
-  .version('0.0.4')
+  .version('0.0.15')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
 
-if (!program.args.length) program.help();
+if (!program.args.length) {
+  console.error('no params given!');
+  process.exit(1);
+}
+
+const [firstConfig, secondConfig] = program.args;
+console.log(gendiff(firstConfig, secondConfig));
