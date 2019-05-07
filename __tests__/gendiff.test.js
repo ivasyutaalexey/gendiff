9a7f643bb9cc31e354a766ff7be47d555633a9e7
a_ivasyuta <ivasyutaalexey@gmail.com>
2019-05-07 12:10:29 +0300

    gendiff json

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,13 @@
 import gendiff from '../src';
 
-test('gendiff', () => {
-  expect(0).toBe(1);
+const fs = require('fs');
+
+test('gendiff json', () => {
+  const diff = gendiff(
+    'C:\\IdeaProjects\\gendiff\\__tests__\\__fixtures__\\file1.json',
+    'C:\\IdeaProjects\\gendiff\\__tests__\\__fixtures__\\file2.json',
+  );
+
+  const result = fs.readFileSync('C:\\IdeaProjects\\gendiff\\__tests__\\__fixtures__\\diff.txt', 'utf8');
+  expect(diff).toBe(result);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-export default (filePath, filePath2) => {
+const fs = require('fs');
 
+const isObjectHasProperty = (context, property) => Object.prototype.hasOwnProperty.call(context, property);
+
+export default (filePath1, filePath2) => {
+  const jsonBefore = JSON.parse(fs.readFileSync(filePath1, 'utf8'));
+  const jsonAfter = JSON.parse(fs.readFileSync(filePath2, 'utf8'));
+  const mergedObject = { ...jsonBefore, ...jsonAfter };
+  const result = ['{\n'];
+
+  Object.keys(mergedObject).forEach((key) => {
+    if (isObjectHasProperty(jsonBefore, key) && isObjectHasProperty(jsonAfter, key)) {
+      if (jsonBefore[key] === jsonAfter[key]) {
+        result.push(`    ${key}: ${jsonAfter[key]}\n`);
+      } else {
+        result.push(`  - ${key}: ${jsonBefore[key]}\n`);
+        result.push(`  + ${key}: ${jsonAfter[key]}\n`);
+      }
+    }
+
+    if (isObjectHasProperty(jsonBefore, key) && !isObjectHasProperty(jsonAfter, key)) {
+      result.push(`  - ${key}: ${jsonBefore[key]}\n`);
+    }
+
+    if (!isObjectHasProperty(jsonBefore, key) && isObjectHasProperty(jsonAfter, key)) {
+      result.push(`  + ${key}: ${jsonAfter[key]}\n`);
+    }
+  });
+
+  result.push('}');
+
+  return result.join('');
 };
