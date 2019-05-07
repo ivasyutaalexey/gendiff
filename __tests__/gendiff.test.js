818ac600e1b6572446fd01fd20352a516657220f
Alexey Ivasyuta <ivasyutaalexey@gmail.com>
2019-05-07 19:53:23 +0300

    unix pathes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 test('gendiff json', () => {
   const diff = gendiff(
-    'C:\\IdeaProjects\\gendiff\\__tests__\\__fixtures__\\file1.json',
-    'C:\\IdeaProjects\\gendiff\\__tests__\\__fixtures__\\file2.json',
+    '__tests__/__fixtures__/file1.json',
+    '__tests__/__fixtures__/file2.json',
   );
 
-  const result = fs.readFileSync('C:\\IdeaProjects\\gendiff\\__tests__\\__fixtures__\\diff.txt', 'utf8');
+  const result = fs.readFileSync('__tests__/__fixtures__/diff.txt', 'utf8');
   expect(diff).toBe(result);
 });
