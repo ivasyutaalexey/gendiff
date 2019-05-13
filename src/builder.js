74c715b5ddb2678237e31dd332ae873bc011e859
Alexey Ivasyuta <ivasyutaalexey@gmail.com>
2019-05-13 22:05:15 +0300

    rename Node function to createNode

--- src/builder.js
+++ src/builder.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const Node = (name, valueBefore, valueAfter, children, status) => ({
+const createNode = (name, valueBefore, valueAfter, children, status) => ({
   name,
   valueBefore,
   valueAfter,
@@ -13,24 +13,24 @@ const builders = [
     check: (obj1, obj2, key) => (_.isObject(obj1[key]) && _.isObject(obj2[key])),
     createNode: (obj1, obj2, key, fn) => {
       const children = fn(obj1[key], obj2[key]);
-      return Node(key, null, null, children, 'node');
+      return createNode(key, null, null, children, 'node');
     },
   },
   {
     check: (obj1, obj2, key) => (!_.has(obj1, key) && _.has(obj2, key)),
-    createNode: (obj1, obj2, key) => Node(key, null, obj2[key], [], 'added'),
+    createNode: (obj1, obj2, key) => createNode(key, null, obj2[key], [], 'added'),
   },
   {
     check: (obj1, obj2, key) => (_.has(obj1, key) && !_.has(obj2, key)),
-    createNode: (obj1, obj2, key) => Node(key, obj1[key], null, [], 'removed'),
+    createNode: (obj1, obj2, key) => createNode(key, obj1[key], null, [], 'removed'),
   },
   {
     check: (obj1, obj2, key) => obj1[key] !== obj2[key],
-    createNode: (obj1, obj2, key) => Node(key, obj1[key], obj2[key], [], 'updated'),
+    createNode: (obj1, obj2, key) => createNode(key, obj1[key], obj2[key], [], 'updated'),
   },
   {
     check: (obj1, obj2, key) => obj1[key] === obj2[key],
-    createNode: (obj1, obj2, key) => Node(key, obj1[key], obj2[key], [], 'unchanged'),
+    createNode: (obj1, obj2, key) => createNode(key, obj1[key], obj2[key], [], 'unchanged'),
   },
 ];
 
